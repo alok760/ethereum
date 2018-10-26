18da4de9fb14cc257a396589ac75de7abf3dfb43
alok <alok760@gmail.com>
2018-10-27 02:28:23 +0530

    basic election structure

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -5,13 +5,22 @@ contract Election {
   struct Candidate {
     uint id;
     string name;
-    uint vote Count;
+    uint voteCount;
   }
   //store candidate
   //fetch candidate
+  mapping(uint => Candidate) public candidates;
   //store candidate
-  string public candidate;
-  function Election () public {
+  uint public candidatesCount;
 
+  function Election () public {
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+
+  }
+
+  function addCandidate (string _name) private {
+    candidatesCount ++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name,0);
   }
 }
